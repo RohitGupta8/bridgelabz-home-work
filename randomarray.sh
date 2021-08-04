41667f659f3753482e25a6f3316de9b01a79e1d9
RohitGupta8 <therohitg@gmail.com>
2021-08-04 10:51:14 +0530

    print 3digit random array

---
+++ randomarray.sh
@@ -0,0 +1,13 @@
+a[0]=$((RANDOM % 1000 ))
+a[1]=$((RANDOM % 1000 ))
+a[2]=$((RANDOM % 1000 ))
+a[3]=$((RANDOM % 1000 ))
+a[4]=$((RANDOM % 1000 ))
+a[5]=$((RANDOM % 1000 ))
+a[6]=$((RANDOM % 1000 ))
+a[7]=$((RANDOM % 1000 ))
+a[8]=$((RANDOM % 1000 ))
+a[9]=$((RANDOM % 1000 ))
+
+echo "Given Random Array : ""[" ${a[@]} "]"
+
